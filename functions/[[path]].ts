dd1e2e579d656345fac4653169ee6db4818e34dd
tung.bui <tung.bui@base.vn>
2025-10-29 15:16:01 +0700

    feat: add gemini 2.0, 2.5 models

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,7 +2,8 @@ import type { ServerBuild } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
-  const serverBuild = (await import('../build/server')) as unknown as ServerBuild;
+  // @ts-ignore - build output may be JS without type declarations
+  const serverBuild = (await import('../build/server.js')) as unknown as ServerBuild;
 
   const handler = createPagesFunctionHandler({
     build: serverBuild,
